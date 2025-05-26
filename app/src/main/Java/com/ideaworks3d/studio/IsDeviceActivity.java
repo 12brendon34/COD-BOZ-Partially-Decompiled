b1e7e1a9178855648a4b4e61e3025c99950d1878
Brendon <43822289+12brendon34@users.noreply.github.com>
2025-05-26 15:13:49 -0700

    Fullscreen

--- app/src/main/Java/com/ideaworks3d/studio/IsDeviceActivity.java
+++ app/src/main/Java/com/ideaworks3d/studio/IsDeviceActivity.java
@@ -3,6 +3,7 @@ package com.ideaworks3d.studio;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 
 import com.ideaworks3d.marmalade.LoaderActivity;
 import com.savegame.SavesRestoring;
@@ -58,9 +59,21 @@ public class IsDeviceActivity extends LoaderActivity {
       Log.v(TAG, "onUserLeaveHint");
       super.onUserLeaveHint();
    }
-
    @Override
    public void onWindowFocusChanged(boolean hasFocus) {
+      super.onWindowFocusChanged(hasFocus);
+
+      if (hasFocus) {
+         getWindow().getDecorView().setSystemUiVisibility(
+                 View.SYSTEM_UI_FLAG_IMMERSIVE
+                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                         | View.SYSTEM_UI_FLAG_FULLSCREEN
+                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+         );
+      }
+
       if (!hasFocus && IsDevice.GetInstance().IsActivated()) {
          IsDevice.GetInstance().IsDeviceKeyCallback(KeyEvent.KEYCODE_HOME);
       }
