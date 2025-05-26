d66ad2607df6f76873bead09c450d16a415cc351
Brendon <43822289+12brendon34@users.noreply.github.com>
2025-05-26 15:19:41 -0700

    Separate deprecated fullscreen

--- app/src/main/Java/com/ideaworks3d/studio/IsDeviceActivity.java
+++ app/src/main/Java/com/ideaworks3d/studio/IsDeviceActivity.java
@@ -1,9 +1,12 @@
 package com.ideaworks3d.studio;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import com.ideaworks3d.marmalade.LoaderActivity;
 import com.savegame.SavesRestoring;
@@ -59,19 +62,37 @@ public class IsDeviceActivity extends LoaderActivity {
       Log.v(TAG, "onUserLeaveHint");
       super.onUserLeaveHint();
    }
+
+   @SuppressWarnings("deprecation")
+   private void applyLegacyImmersiveMode() {
+      int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_IMMERSIVE;
+
+       getWindow().getDecorView().setSystemUiVisibility(flags);
+   }
+
+
    @Override
    public void onWindowFocusChanged(boolean hasFocus) {
       super.onWindowFocusChanged(hasFocus);
 
       if (hasFocus) {
-         getWindow().getDecorView().setSystemUiVisibility(
-                 View.SYSTEM_UI_FLAG_IMMERSIVE
-                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-         );
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11+ (API 30+)
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+
+            if (insetsController != null) {
+               insetsController.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+               insetsController.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+            getWindow().setDecorFitsSystemWindows(false);
+         } else {
+            applyLegacyImmersiveMode();
+         }
       }
 
       if (!hasFocus && IsDevice.GetInstance().IsActivated()) {
